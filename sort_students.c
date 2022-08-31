f421d683764d0a9f56c0b5f9ed88eae077f4fb86
Ayush Khatri <ayushkhatri362@gmail.com>
2022-08-31 20:10:14 +0530

    Modified sort_students and added name programs

--- sort_students.c
+++ sort_students.c
@@ -1,31 +1,56 @@
 #include <stdio.h>
+#include <string.h>
 
 struct Student
 {
     char *name;
     int marks[5];
+    int total;
 };
 
+int calculate_marks(struct Student student)
+{
+    int i, marks = 0;
+    for (i = 0; i < 5; i++)
+    {
+        marks = marks + student.marks[i];
+    }
+    return marks;
+}
 // Sort this by marks of students in descending order
 
 int main(void)
 {
     struct Student students[5] = {
         {"Sangeet", {50, 60, 70, 80, 90}},
-        {"Ayush", {88, 55, 77, 45, 86}},
         {"Dev", {85, 58, 98, 57, 82}},
         {"Anil", {58, 75, 66, 99, 66}},
+        {"Ayush", {88, 55, 77, 45, 86}}, 
         {"Anand", {52, 66, 80, 70, 85}}};
-
+    // Make changes here
     int i;
     for (i = 0; i < 5; i++)
     {
-        int j, marks = 0;
-        for (j = 0; j < 5; j++)
+        students[i].total = calculate_marks(students[i]);
+    }
+    i = 0;
+    for (i = 0; i < 5; i++)
+    {
+        int j;
+        for (j = 0; j < 5-1; j++)
         {
-            marks = marks + students[i].marks[j];
+            struct Student temp;
+            if (students[j].total < students[j + 1].total)
+            {
+                strcpy(temp.name, students[j].name);
+                strcpy(students[j].name, students[j + 1].name);
+                strcpy(students[j + 1].name, temp.name);
+                temp.total = students[j].total;
+                students[j].total = students[j + 1].total;
+                students[j + 1].total = temp.total;
+                
+            }
         }
-        // Make changes here
-        printf("%s:%d\n", students[i].name, marks);
+        // printf("%s:%d\n", students[i].name, students[i].total);
     }
 }
