9c100ca48aaa71c7e9388036cc8caccacc6db6fe
Sangeet Khatri <sangeet.khatri@outlook.com>
2022-08-31 20:41:48 +0530

    Modified sort_students.c and removed all compiled files

--- sort_students.c
+++ sort_students.c
@@ -1,56 +1,58 @@
 #include <stdio.h>
 #include <string.h>
 
-struct Student
-{
-    char *name;
+struct Student {
+    char name[20];
     int marks[5];
     int total;
 };
 
-int calculate_marks(struct Student student)
-{
+int calculate_marks(struct Student student){
     int i, marks = 0;
-    for (i = 0; i < 5; i++)
-    {
+    for (i = 0; i < 5; i++){
         marks = marks + student.marks[i];
     }
     return marks;
 }
-// Sort this by marks of students in descending order
 
-int main(void)
-{
+void print_student(struct Student s){
+    printf("%s has total %d\n", s.name, s.total);
+}
+
+int main(void) {
     struct Student students[5] = {
         {"Sangeet", {50, 60, 70, 80, 90}},
         {"Dev", {85, 58, 98, 57, 82}},
         {"Anil", {58, 75, 66, 99, 66}},
         {"Ayush", {88, 55, 77, 45, 86}}, 
-        {"Anand", {52, 66, 80, 70, 85}}};
-    // Make changes here
-    int i;
-    for (i = 0; i < 5; i++)
-    {
+        {"Anand", {52, 66, 80, 70, 85}}
+    };
+
+    for (int i = 0; i < 5; i++){
         students[i].total = calculate_marks(students[i]);
     }
-    i = 0;
-    for (i = 0; i < 5; i++)
-    {
-        int j;
-        for (j = 0; j < 5-1; j++)
-        {
+
+
+    for (int i = 0; i < 5; i++) {
+        for (int j = 0; j < 5-1; j++) {
             struct Student temp;
-            if (students[j].total < students[j + 1].total)
-            {
-                strcpy(temp.name, students[j].name);
-                strcpy(students[j].name, students[j + 1].name);
-                strcpy(students[j + 1].name, temp.name);
+            if (students[j].total < students[j + 1].total) {
+
+                strcpy(temp.name ,students[j].name);
                 temp.total = students[j].total;
+    
+                strcpy(students[j].name ,students[j + 1].name);
                 students[j].total = students[j + 1].total;
+    
+                strcpy(students[j + 1].name, temp.name);
                 students[j + 1].total = temp.total;
                 
             }
         }
-        // printf("%s:%d\n", students[i].name, students[i].total);
+    }
+
+    for(int i=0;i<5;i++){
+        print_student(students[i]);
     }
 }
+
