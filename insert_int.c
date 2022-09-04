c6ab9ebf4bf48e8067e52dc3df22f7bbfbbed1e0
Ayush Khatri <ayushkhatri362@gmail.com>
2022-09-04 14:45:42 +0530

    Insertion sort of integer program added

---
+++ insert_int.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+void print_array(int *nums,int length)
+{
+    for(int i=0;i<length;i++)
+    {
+        printf("%d ",nums[i]);
+    }
+}
+
+void swap(int *n1,int *n2)
+{
+    int temp;
+    temp=*n1;
+    *n1=*n2;
+    *n2=temp;
+}
+
+int main(void)
+{
+    int length = 10;
+    int nums[10] = {10, 7, 8, 5, 2, 4, 3, 1, 9, 6};
+    //int nums[10] ={10,7,6};
+    for (int i = 1; i < length; i++)
+    {
+        for (int j = 0; j <i-1; j++)
+        {
+            if(nums[i]<nums[j])
+            {
+                swap(&nums[i],&nums[j]);
+            }
+        }
+    }
+    print_array(nums,length);
+}
